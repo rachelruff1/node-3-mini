c524d22c27a9576bacc5bb2e3a14f5b609c50998
Rachel Ruff <rachelruff@Rachels-MacBook-Pro.local>
2018-02-13 12:27:00 -0600

    finished demo

--- server/index.js
+++ server/index.js
@@ -1,17 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mc = require( `./controllers/messages_controller` );
-
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mc = require(`./controllers/messages_controller`);
+const session = require("express-session");
+const createInitialSession = require(`${__dirname}/middlewares/session`);
+const filter = require(`${__dirname}/middlewares/filter`);
 const app = express();
 
-app.use( bodyParser.json() );
-app.use( express.static( `${__dirname}/../build` ) );
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../build`));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: { maxAge: 10000 }
+  })
+);
+
+app.use((req, res, next) => createInitialSession(req, res, next));
+app.use((req, res, next) => {
+  if (req.method === "PUT" || req.method === "POST") {
+    filter(req, res, next);
+  } else {
+    next();
+  }
+});
 
 const messagesBaseUrl = "/api/messages";
-app.post( messagesBaseUrl, mc.create );
-app.get( messagesBaseUrl, mc.read );
-app.put( `${messagesBaseUrl}`, mc.update );
-app.delete( `${messagesBaseUrl}`, mc.delete );
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}`, mc.update);
+app.delete(`${messagesBaseUrl}`, mc.delete);
+app.get(`${messagesBaseUrl}/history`, mc.history);
 
-const port = process.env.PORT || 3000
-app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
